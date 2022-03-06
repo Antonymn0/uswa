6d22c79f6fb5ed6103a937b98318fd66a684a188
antonymn0 <mnantony0@gmail.com>
2022-03-06 12:03:57 +0300

    dkd

--- resources/js/app/components/Notifications/TutorSignupProcessNotification.vue
+++ resources/js/app/components/Notifications/TutorSignupProcessNotification.vue
@@ -1,6 +1,6 @@
 <template>
   <div >    
-    <div v-if="this.show_message" class="bg-secondary text-white py-2 px-3 border-bottom position-fixed w-100 top-0" >
+    <div v-if="this.show_message && this.getUser.first_name" class="bg-secondary text-white py-2 px-3 border-bottom position-fixed w-100 top-0" >
         <span> {{getUser.first_name}}, Your account registration is pending. Please complete the tutor signup process to start geting bids from students. <br>
             Click this button to complete the process.
             <button class="btn btn-secondary px-4 rounded " style="background-color:#394e4e;" @click.prevent="this.$router.push({name: 'register-tutor-signup'})">Complete signup</button>
@@ -19,6 +19,7 @@ export default {
     },
     methods:{       
         is_account_registration_complete(){
+            this.show_message = false;
             setTimeout(() => {                                              
                 if(this.getUser.registration == 'complete') return;
                 if(this.getUser.role !== 'tutor') return;
