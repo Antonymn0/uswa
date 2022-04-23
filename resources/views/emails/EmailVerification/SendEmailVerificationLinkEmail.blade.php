dbc265c6de082946bdb25dcb4f306fc148e39378
antonymn0 <mnantony0@gmail.com>
2022-04-23 07:31:25 +0300

    fix email images bugs

--- resources/views/emails/EmailVerification/SendEmailVerificationLinkEmail.blade.php
+++ resources/views/emails/EmailVerification/SendEmailVerificationLinkEmail.blade.php
@@ -1,6 +1,6 @@
 @component('mail::message')
 <img src="https://uswa-app.herokuapp.com/images/cover.png" style="width:90px; float:right" alt="uswa Logo">
-<br>
+<br>  
 <h1>  Verify your email </h1>
     
 <h3> Hello {{$user->first_name}},</h3> 
