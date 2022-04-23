440b5156305bc5c139430428ecabcffbe896aa7a
antonymn0 <mnantony0@gmail.com>
2022-04-23 07:05:43 +0300

    fix email images bugs

--- resources/views/emails/transaction/transactionProcessed.blade.php
+++ resources/views/emails/transaction/transactionProcessed.blade.php
@@ -1,5 +1,5 @@
 @component('mail::message')
-<img src="{{url('https://uswa-app.herokuapp.com/images/logo.svg')}}" style="width:90px; float:right" alt="menuthy Logo">
+<img src="{{ $message->embed('https://uswa-app.herokuapp.com/images/logo.svg')  }}" style="width:90px; float:right" alt="menuthy Logo">
 <br>
 <h2>Dear user, </h2> 
 
